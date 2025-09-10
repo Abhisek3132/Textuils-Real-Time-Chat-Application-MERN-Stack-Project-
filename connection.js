5ade5f1f4adb9fba19b715aacd4aadc8a60e3bb9
Abhisek3132 <abhskchkrbrt3132@gmail.com>
2025-09-11 01:09:46 +0530

    Delete connection.js

--- connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const url = `mongodb+srv://chat_app_admin:admin1234@cluster0.pamb9fw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() =>
-    console.log('Connected to MongoDB')).catch((e)=> console.log('Error connecting to MongoDB:', e));
